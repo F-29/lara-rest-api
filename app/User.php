f9352aa8f826871111d6ff42c3a18e6815699584
Mosich <m.chegeni7979@gmail.com>
2020-01-13 22:49:09 +0330

    Initial Commit

---
+++ app/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App;
+
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
+
+/**
+ * Class User
+ * @package App
+ * @inheritDoc Authenticatable
+ */
+class User extends Authenticatable
+{
+    use Notifiable, HasApiTokens;
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function logout()
+    {
+        $this->api_token = null;
+        $this->save();
+        return $this;
+    }
+
+    public function generateApiToken()
+    {
+        $token = Str::random(128);
+        $this->api_token = $token;
+        $this->save();
+        return $token;
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'api_token',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token', 'api_token', 'user_verified_at'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Article;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/form", function () {
+    return view("form");
+});
+
+Route::get("/user-form", function () {
+    return view("user-form");
+});
